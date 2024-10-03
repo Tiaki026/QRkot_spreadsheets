b49b35b9cbf0353ac81b9935f69f67eaafdba722
Eugene Kolotikov <jeka.pxb@yandex.ru>
2024-10-03 16:00:36 +0300

    Commit

--- alembic/env.py
+++ alembic/env.py
@@ -3,10 +3,9 @@ import os
 from logging.config import fileConfig
 
 from dotenv import load_dotenv
-from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 from sqlalchemy.engine import Connection
-from sqlalchemy.ext.asyncio import async_engine_from_config, AsyncEngine
+from sqlalchemy.ext.asyncio import async_engine_from_config
 from app.core.base import Base
 
 from alembic import context
